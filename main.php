537213b057daa13607907952069c92734457cb78
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 12:36:23 +0530

    Initial commit

---
+++ main.php
@@ -0,0 +1,21 @@
+$message = '
+<html>
+<head>
+  <title>Review Request Reminder</title>
+</head>
+<body>
+  <p>Here are the cases requiring your review in December:</p>
+  <table>
+    <tr>
+      <th>Case title</th><th>Category</th><th>Status</th><th>Due date</th>
+    </tr>
+    <tr>
+      <td>Case 1</td><td>Development</td><td>pending</td><td>Dec-20</td>
+    </tr>
+    <tr>
+      <td>Case 1</td><td>DevOps</td><td>pending</td><td>Dec-21</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
